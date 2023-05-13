acfb4d680abe25ef7ea21f9aa885d698563f4a98
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-13 23:50:00 +0300

    Add Stack Screen component

--- app/index.js
+++ app/index.js
@@ -6,12 +6,24 @@ import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components
 
 
 const Home  = ()=>{
+    const router = useRouter
+
     return (
-        <View>
-            <Text>
-                Home
-            </Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension='100%' />
+          ),
+          headerTitle: "",
+        }}
+      />
+    </SafeAreaView>
     );
 }
 
